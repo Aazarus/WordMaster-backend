f281e1ac50441e28476b8b683160b32d017ee9fb
Aaron Moreland <aaron@sevnasoftware.com>
2025-02-05 23:31:50 +0000

    Corrected allowed origins

--- src/allowedOrigins.ts
+++ src/allowedOrigins.ts
@@ -1,1 +1,1 @@
-export const allowedOrigins = ['https://wordmaster.sevnasoftware.com/', 'http://localhost:8100', 'http://localhost:8080', 'http://localhost:5173'];
+export const allowedOrigins = ['https://wordmaster.sevnasoftware.com/', 'http://localhost:8100/', 'http://localhost:8080/', 'http://localhost:5173/'];
